7deaef0c5b9bf5e5f4cb35a5281982e819084a34
Vladislav Mitskevich <mitskevichvlm@gmail.com>
2022-12-24 18:28:30 +0300

    feat: add souton for Task01 lesson02

---
+++ src/main/java/lesson02/Task01.java
@@ -0,0 +1,10 @@
+package lesson02;
+
+public class Task01 {
+    public static void main(String[] args) {
+        int divisible = 9;
+        int divisor = 5;
+        System.out.printf("division result: %s / %s = %s\n", divisible, divisor, divisible/divisor);
+        System.out.printf("the remainder of the division: %s / %s = %s", divisible, divisor, divisible%divisor);
+    }
+}
